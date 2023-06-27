42f7bd8ea5cd092339d34301ebf6854cd16d62c9
Saqib Islam <saaqi.grw@gmail.com>
2023-06-27 23:16:40 +0500

    assets folder upload

---
+++ assets/js/main.js
@@ -0,0 +1,304 @@
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('body').classList.toggle('mobile-nav-active')
+    this.classList.toggle('bx-menu')
+    this.classList.toggle('bx-x')
+  })
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let body = select('body')
+      if (body.classList.contains('mobile-nav-active')) {
+        body.classList.remove('mobile-nav-active')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bx-menu')
+        navbarToggle.classList.toggle('bx-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+
+  /**
+   * Skills animation
+   */
+  let skilsContent = select('.skills-content');
+  if (skilsContent) {
+    new Waypoint({
+      element: skilsContent,
+      offset: '80%',
+      handler: function(direction) {
+        let progress = select('.progress .progress-bar', true);
+        progress.forEach((el) => {
+          el.style.width = el.getAttribute('aria-valuenow') + '%'
+        });
+      }
+    })
+  }
+
+
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Initiate portfolio details lightbox 
+   */
+  const portfolioDetailsLightbox = GLightbox({
+    selector: '.portfolio-details-lightbox',
+    width: '90%',
+    height: '90vh'
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Testimonials slider
+   */
+  new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+
+})()
+
+
+// JavaScript form submission
+const form = document.getElementById("php-email-form");
+
+form.addEventListener("submit", function(event) {
+    const nameInput = document.getElementById("name");
+    const emailInput = document.getElementById("email");
+    const messageInput = document.getElementById("message");
+    let isValid = true;
+
+    // Validate name input
+    if (nameInput.value === "") {
+      isValid = false;
+      nameInput.setCustomValidity("Please enter your name.");
+    } else {
+      nameInput.setCustomValidity("");
+    }
+
+    // Validate email input
+    if (emailInput.value === "") {
+      isValid = false;
+      emailInput.setCustomValidity("Please enter your email address.");
+    } else if (!emailInput.checkValidity()) {
+      isValid = false;
+      emailInput.setCustomValidity("Please enter a valid email address.");
+    } else {
+      emailInput.setCustomValidity("");
+    }
+
+    // Validate message input
+    if (messageInput.value === "") {
+      isValid = false;
+      messageInput.setCustomValidity("Please enter a message.");
+    } else {
+      messageInput.setCustomValidity("");
+    }
+
+    // Submit form if all inputs are valid
+    if (!isValid) {
+      event.preventDefault();
+    }
+});
+
+
+form.addEventListener("submit", function(event) {
+    const nameInput = document.getElementById("name");
+    const emailInput = document.getElementById("email");
+    const subjectInput = document.getElementById("subject");
+    const messageInput = document.getElementById("message");
+    const receiveEmail = "saaqi.grw@gmail.com"
+    const emailButton = document.querySelector('button.bttn');
+
+    // Compose email message
+    const subject = `${subjectInput.value} from ${nameInput.value}`;
+    const body = `${messageInput.value}\n\n${nameInput.value}\n${emailInput.value}`;
+
+    // Open default email app and fill in appropriate fields
+    const mailtoUrl = `mailto:${encodeURIComponent(receiveEmail)}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+    // Open Email client on click
+    window.open(mailtoUrl);
+
+    // Prevent default form submission
+    event.preventDefault();
+});
+
+
+// animate counters
+function countWhenVisible(element, targetCount, speed) {
+  let hasCounted = false;
+  let startTime = null;
+  let observer = new IntersectionObserver(entries => {
+    if (entries[0].isIntersecting && !hasCounted) {
+      hasCounted = true;
+      startTime = performance.now();
+      let count = 0;
+      let duration = speed;
+      let interval = setInterval(() => {
+        let elapsedTime = performance.now() - startTime;
+        let progress = elapsedTime / duration;
+        if (progress >= 1) {
+          clearInterval(interval);
+          element.innerHTML = targetCount;
+        } else {
+          count = Math.floor(progress * targetCount);
+          element.innerHTML = count;
+        }
+      }, 20);
+    }
+  });
+  observer.observe(element);
+}
+countWhenVisible(document.querySelector(".purecounter.happy"), 22, 2000);
+countWhenVisible(document.querySelector(".purecounter.project"), 59, 2000);
+countWhenVisible(document.querySelector(".purecounter.support"), 1463, 2000);
+countWhenVisible(document.querySelector(".purecounter.certificate"), 8, 2000);
+
+
+
+
+
+
+/**
+ * Hide Preloader
+ */
+let preloader = document.getElementById('preloader');
+if (preloader) {
+  window.addEventListener('load', () => {
+    preloader.remove();
+  });
+}
