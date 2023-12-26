e7bc1f6d2b5bcb691785011de799008c60fb2e3a
Saqib Islam <saaqi.grw@gmail.com>
2023-12-27 00:28:26 +0500

    minor

--- src/js/skill-waypoints.js
+++ src/js/skill-waypoints.js
@@ -11,13 +11,13 @@ import "waypoints/lib/noframework.waypoints";
   /**
    * Skills animation
    */
-  let skilsContent = select(".skills-content");
+  let skilsContent = document.querySelector(".skills-content");
   if (skilsContent) {
     new Waypoint({
       element: skilsContent,
       offset: "80%",
       handler: function (direction) {
-        let progress = select(".progress .progress-bar", true);
+        let progress = document.querySelectorAll(".progress .progress-bar", true);
         progress.forEach((el) => {
           el.style.width = el.getAttribute("aria-valuenow") + "%";
         });
