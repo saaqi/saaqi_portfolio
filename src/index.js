f5cf6f02867ca8425b9be4865104995f735b268a
Saqib Islam <saaqi.grw@gmail.com>
2023-12-29 21:46:45 +0500

    patch fixes

--- src/index.js
+++ src/index.js
@@ -25,18 +25,3 @@ import "./js/hide-preloader"
 
 // Import Lightbox
 import "./js/lightbox"
-
-
-// --- Import only the required components.
-// import { Alert } from 'bootstrap';
-// import { Button } from 'bootstrap';
-// import { Carousel } from "bootstrap";
-// import { Collapse } from "bootstrap";
-// import { Dropdown } from "bootstrap";
-// import { Modal } from "bootstrap";
-// import { Offcanvas } from "bootstrap";
-// import { Popover } from "bootstrap";
-// import { ScrollSpy } from "bootstrap";
-// import { Tab } from "bootstrap";
-// import { Toast } from "bootstrap";
-// import { Tooltip } from "bootstrap";
