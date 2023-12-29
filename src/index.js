c79a578b3909b291eba9d7357a106980c28c6489
Saqib Islam <saaqi.grw@gmail.com>
2023-12-29 17:05:39 +0500

    patch fixes

--- src/index.js
+++ src/index.js
@@ -15,12 +15,28 @@ import "boxicons/css/boxicons.css"
 
 /* ## Import JS
 --------------------------------------------- */
-const IN_PRODUCTION = process.env.NODE_ENV === "production";
-
-import "./js/interact"
+import "./js/navigation"
 import "./js/animate-counter"
 import "./js/skill-waypoints"
 import "./js/contact-form"
 import "./js/top-link"
 import "./js/date"
 import "./js/hide-preloader"
+
+// Import Lightbox
+// import "./js/lightbox"
+
+
+// --- Import only the required components.
+// import { Alert } from 'bootstrap';
+// import { Button } from 'bootstrap';
+// import { Carousel } from "bootstrap";
+// import { Collapse } from "bootstrap";
+// import { Dropdown } from "bootstrap";
+// import { Modal } from "bootstrap";
+// import { Offcanvas } from "bootstrap";
+// import { Popover } from "bootstrap";
+// import { ScrollSpy } from "bootstrap";
+// import { Tab } from "bootstrap";
+// import { Toast } from "bootstrap";
+// import { Tooltip } from "bootstrap";
