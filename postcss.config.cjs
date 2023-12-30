2f08c37a7194cf9e698fbd7baf9dc7b2eedc439b
Saqib Islam <saaqi.grw@gmail.com>
2023-12-30 13:16:36 +0500

    patch fixes

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
       content: [
         "index.html",
         "./src/**/*.js",
-        `${bootstrap}/dist/js/bootstrap.bundle.min.js`,
+        // `${bootstrap}/dist/js/bootstrap.bundle.min.js`,
       ],
       defaultExtractor(content) {
         const contentWithoutStyleBlocks = content.replace(
--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -1,1 +1,5 @@
+// Import Bootstrap's Modal JS
 import "bootstrap/js/dist/modal"
+
+// Include the classes added by Bootstrap Modal to keep them from purging on build from stylesheet ;)
+const keepStyles = document.querySelectorAll(".modal-backdrop, .fadm .show .modal-open");
