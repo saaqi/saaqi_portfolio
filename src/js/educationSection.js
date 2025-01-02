80614bb166fc49fb9fd4f52869b4e8d3b327a86f
Saqib Islam <saaqi.grw@gmail.com>
2025-01-02 21:39:46 +0500

    adjustments

--- src/js/educationSection.js
+++ src/js/educationSection.js
@@ -1,5 +1,5 @@
 import certificatesList from "./functions/certificatesList.js"
-import draggableContainer from "./functions/draggableContainer.js"
+// import draggableContainer from "./functions/draggableContainer.js"
 
 certificatesList('certificates-container')
-draggableContainer('certificates-container')
+// draggableContainer('certificates-container')
--- src/js/portfolioSection.js
+++ src/js/portfolioSection.js
@@ -1,6 +1,6 @@
 import projectsList from "./functions/projectsLists.js"
-import draggableContainer from "./functions/draggableContainer.js"
+// import draggableContainer from "./functions/draggableContainer.js"
 
 
 projectsList('portfolio-container')
-draggableContainer('portfolio-container')
+// draggableContainer('portfolio-container')
