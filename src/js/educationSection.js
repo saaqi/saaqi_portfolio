dc1d089f04ad2bbdd0bf3255ad2852c86781c0c7
Saqib Islam <saaqi.grw@gmail.com>
2025-01-02 20:58:14 +0500

    test

--- src/js/educationSection.js
+++ src/js/educationSection.js
@@ -2,4 +2,4 @@ import certificatesList from "./functions/certificatesList.js"
 import draggableContainer from "./functions/draggableContainer.js"
 
 certificatesList('certificates-container')
-draggableContainer('certificates-container')
+// draggableContainer('certificates-container')
