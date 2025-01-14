a23481dce9c2d5859847cb3ad6a3a03daa189113
Saqib Islam <saaqi.grw@gmail.com>
2025-01-14 16:38:58 +0500

    Update site manifest, improve modal styles, and remove unused HTML

--- src/js/sectionServices.js
+++ src/js/sectionServices.js
@@ -1,6 +1,8 @@
+import 'bootstrap/js/dist/modal.js'
+// ".modal-backdrop, .fad, .show, .modal-open"
+
 // Import Styles
 import "../styles/sectionServices.scss"
-
 
 import draggableContainer from "./functions/draggableContainer.js"
 draggableContainer('services-container')
@@ -33,26 +35,30 @@ const servicesLists = selectorID => {
           </div>
           <div class="card-footer">
           ${ more && `<div class="btn-group w-100">`}
-            <a href="#contact" class="btn btn-outline-secondary w-50 btn-icon scrollto" title="Hire Me!">
+            <a href="#contact" class="btn btn-outline-secondary w-50 btn-icon d-flex align-items-center justify-content-center gap-1 scrollto" title="Hire Me!">
               <svg class="icon handshake-icon"><use href="#handshake-icon"></use></svg> Hire Me!
             </a>
             ${more &&
-            `<button type="button" class="btn btn-outline-primary w-50 btn-icon" data-bs-toggle="modal" data-bs-target="#${`service-` + index}" title="Get More Info">
-              <svg class="icon content-icon"><use href="#content-icon"></use></svg> More
+            `<button type="button" class="btn btn-outline-primary w-50 btn-icon d-flex align-items-center justify-content-center gap-1" data-bs-toggle="modal" data-bs-target="#${`service-` + index}" title="Get More Info">
+              <svg class="icon content-icon"><use href="#content-icon"></use></svg> Info
             </button>`}
           ${ more && `</div>`}
           </div>
         </div>
         ${more &&
-        `<div class="modal fade modal-fullscreen modal-xl" id="${`service-` + index}" tabindex="-1" aria-labelledby="${`service-label-` + index}"
-          aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        `<div class="modal fade" id="${`service-` + index}" tabindex="-1" aria-labelledby="${`service-` + index}" aria-hidden="true">
+          <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
             <div class="modal-content">
               <div class="modal-header">
-                <div class="modal-title fs-5" id="${`service-label-` + index}">${title}</div>
+                <div class="modal-title fs-5" id="${`service-` + index}">${title}</div>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div class="modal-body">${more}</div>
+              <div class="modal-footer py-1">
+                <button type="button" class="btn btn-danger py-2 px-3 d-flex align-items-center" data-bs-dismiss="modal">
+                  <svg class="icon close-icon fs-4"><use href="#close-icon"></use></svg> Close
+                </button>
+              </div>
             </div>
           </div>
         </div>`}
